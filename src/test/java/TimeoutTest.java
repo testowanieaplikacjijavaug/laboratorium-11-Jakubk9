b9eac220bf842c7b08e80918a5f70555663b0a20
Jakub Kisłowski <35227458+Jakubk9@users.noreply.github.com>
2020-05-04 15:06:26 +0200

    zad4

---
+++ src/test/java/TimeoutTest.java
@@ -0,0 +1,54 @@
+import io.github.bonigarcia.seljup.SeleniumExtension;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ExtendWith(SeleniumExtension.class)
+public class TimeoutTest {
+    private WebDriver webDriver1;
+    private WebDriver webDriver2;
+
+    public TimeoutTest(ChromeDriver chromeDriver) {
+        this.webDriver1 = chromeDriver;
+        this.webDriver2 = chromeDriver;
+    }
+
+    @BeforeEach
+    public void setUp() {
+        webDriver1.get("https://duckduckgo.com/");
+        webDriver1.manage()
+                .timeouts()
+                .pageLoadTimeout(10,TimeUnit.SECONDS);
+        webDriver2.get("https://duckduckgo.com/");
+        webDriver2.manage()
+                .timeouts()
+                .setScriptTimeout(10,TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void pageLoadTimeoutTest() {
+        webDriver1.findElement(By.id("search_form_input_homepage"))
+                .sendKeys("Testowanie");
+        webDriver1.findElement(By.id("search_button_homepage"))
+                .click();
+
+        assertTrue(webDriver1.getTitle().contains("Testowanie"));
+    }
+
+    @Test
+    public void setScriptTimeoutTest() {
+        webDriver1.findElement(By.id("search_form_input_homepage"))
+                .sendKeys("Testowanie");
+        webDriver1.findElement(By.id("search_button_homepage"))
+                .click();
+
+        assertTrue(webDriver1.getTitle().contains("Testowanie"));
+    }
+}
